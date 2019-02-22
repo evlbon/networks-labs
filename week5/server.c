419c75cc74d46200bbdf7a62179c27ac6530045c
potemin1999 <potemin1999@bk.ru>
2019-02-22 10:28:34 +0300

    multithreading added

--- week5/server.c
+++ week5/server.c
@@ -7,36 +7,42 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 #include <zconf.h>
 
+#define THREAD_COUNT    4
 #define SERVER_PORT     2000
 
+pthread_t        workers[THREAD_COUNT];
 char             data_buffer[1024];
 struct student_t student;
+int              counter;
 
-int process_connection(int sockfd) {
+void *process_connection(void *data) {
     struct sockaddr_in client_addr;
-    ssize_t            sent_recv_bytes = 0;
-    socklen_t          addr_len        = sizeof(client_addr);
+
+    socklen_t addr_len  = sizeof(client_addr);
+    pthread_t thread    = pthread_self();
+    ssize_t   sent_recv_bytes;
+    int       *data_i   = (int *) data;
+    int       sockfd    = *(data_i + 0);
+    int       worker_id = (int) thread;
+
     memset(&client_addr, 0, sizeof(client_addr));
     while (1) {
-        printf("Server ready to service client msgs.\n");
+        printf("Worker %i ready to service client msgs.\n", worker_id);
         memset(data_buffer, 0, sizeof(data_buffer));
         sent_recv_bytes = recvfrom(sockfd, (char *) &student, sizeof(struct student_t), 0,
                                    (struct sockaddr *) &client_addr, &addr_len);
-        printf("Server recvd %zi bytes from client %s:%u\n", sent_recv_bytes,
+        printf("Worker %i recvd %zi bytes from client %s:%u\n", worker_id, sent_recv_bytes,
                inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-        if (sent_recv_bytes == 0) {
-            close(sockfd);
-            break;
-        }
         //change student...
         bzero(student.group, 16);
         strcpy(student.group, "Dropped");
         //send him back
         sent_recv_bytes = sendto(sockfd, (char *) &student, sizeof(struct student_t), 0,
                                  (struct sockaddr *) &client_addr, addr_len);
-        printf("Server sent %zi bytes in reply to client\n", sent_recv_bytes);
+        printf("Worker %i sent %zi bytes in reply to client\n", worker_id, sent_recv_bytes);
     }
 }
 
@@ -49,8 +55,8 @@ int main(int argc, char **argv) {
     }
     int optval = 1;
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
-               (const void *)&optval, sizeof(int));
-    bzero((char*) &server_addr, sizeof(server_addr));
+               (const void *) &optval, sizeof(int));
+    bzero((char *) &server_addr, sizeof(server_addr));
     server_addr.sin_family      = AF_INET;
     server_addr.sin_port        = SERVER_PORT;
     server_addr.sin_addr.s_addr = INADDR_ANY;
@@ -58,8 +64,13 @@ int main(int argc, char **argv) {
         printf("socket bind failed\n");
         return 0;
     }
-    while (1) {
-        process_connection(sockfd);
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        int data[1];
+        data[0] = sockfd;
+        pthread_create(&workers[i], 0, process_connection, &data);
+    }
+    for (int j = 0; j < THREAD_COUNT; ++j) {
+        pthread_join(workers[j], 0);
     }
     return 0;
 }
